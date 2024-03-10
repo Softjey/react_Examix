c84823f6ed5b14086657be3b0a6c5816775060a3
macsbook <maks.naumovich@gmail.com>
2024-03-10 12:25:09 +0100

    fix styles and layout, refactor unused components

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,66 +1,68 @@
 import { useNavigate } from 'react-router';
-import { Button, Fab, TextField } from '@mui/material';
+import { Fab, TextField } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Layout from '../Layout';
+import MainButton from '../components/MainButton';
 
-const Inputs: React.FC = () => (
-  <div
-    css={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      gap: '15px',
-      width: '300px',
-    }}
-  >
-    <TextField label="Name" placeholder="Enter name" variant="outlined" />
-    <TextField
-      inputProps={{ maxLength: 6 }}
-      label="Code"
-      placeholder="Enter game code"
-      variant="outlined"
-    />
-  </div>
-);
-
-const Wrapper: React.FC = () => {
+const JoinPage: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <div css={{ position: 'relative' }}>
-      <h1 css={{ marginBottom: '20px' }}>Examix</h1>
-      <form
+    <Layout>
+      <div
         css={{
+          height: '300px',
+          gap: '20px',
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'center',
-          gap: '20px',
         }}
       >
-        <Inputs />
-        <Button sx={{ width: '200px', height: '50px', fontSize: '1rem' }} variant="contained">
-          Join the test
-        </Button>
-      </form>
-      <Fab
-        onClick={() => navigate('/')}
-        color="primary"
-        size="large"
-        sx={{
-          position: 'absolute',
-          top: '7px',
-          left: '-70px',
-        }}
-      >
-        <ArrowBackIcon />
-      </Fab>
-    </div>
+        <header css={{ position: 'relative' }}>
+          <h1>Examix</h1>
+          <Fab
+            onClick={() => navigate('/')}
+            color="primary"
+            size="large"
+            sx={{
+              position: 'absolute',
+              transform: 'translateY(-50%)',
+              top: '50%',
+              // is needed to center button inside header
+              left: '-70px',
+            }}
+          >
+            <ArrowBackIcon />
+          </Fab>
+        </header>
+        <form
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '20px',
+          }}
+        >
+          <div
+            css={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              gap: '15px',
+              width: '300px',
+            }}
+          >
+            <TextField label="Name" placeholder="Enter name" variant="outlined" />
+            <TextField
+              inputProps={{ maxLength: 6 }}
+              label="Code"
+              placeholder="Enter game code"
+              variant="outlined"
+            />
+          </div>
+          <MainButton variant="contained">Join the test</MainButton>
+        </form>
+      </div>
+    </Layout>
   );
 };
-
-const JoinPage: React.FC = () => (
-  <Layout>
-    <Wrapper />
-  </Layout>
-);
 
 export default JoinPage;
