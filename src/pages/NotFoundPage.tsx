86a08746c933fc154bf3946755810af01cb0c690
macsbook <maks.naumovich@gmail.com>
2024-03-10 11:54:02 +0100

    fix styles, add h1 - h6 sizes in index.css

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -7,7 +7,7 @@ const NotFoundPage: React.FC = () => {
   return (
     <Layout>
       <h1 css={{ fontSize: '6.25rem' }}>404</h1>
-      <h2 css={{ fontSize: '3.5rem', marginBottom: '1.25rem' }}>Page Not Found</h2>
+      <h2 css={{ marginBottom: '20px' }}>Page Not Found</h2>
       <Button onClick={() => navigate('/')} variant="contained" size="large">
         Go Home
       </Button>
