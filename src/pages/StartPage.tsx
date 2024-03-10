21a9a9b7b3f37ec93f35bc1bc7857247eb44c9aa
macsbook <maks.naumovich@gmail.com>
2024-03-10 10:23:50 +0100

    fix styles and layout, create modified component of ui button in src/components/MainButton.tsx, fix text in join the test button

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,27 +1,29 @@
-import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import MainButton from '../components/MainButton';
 import Layout from '../Layout';
 
 const StartPage: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <Layout style={{ gap: '25px', transform: 'translateY(-5%)' }}>
-      <h1>Examix</h1>
-      <div css={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
-        <Button
-          onClick={() => navigate('/login')}
-          css={{ width: '200px', height: '50px', fontSize: '1rem' }}
-          variant="contained"
-        >
-          Login as teacher
-        </Button>
-        <Button
-          onClick={() => navigate('/join')}
-          css={{ width: '200px', height: '50px', fontSize: '1rem' }}
-          variant="contained"
-        >
-          Login as student
-        </Button>
+    <Layout>
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '25px',
+          height: '300px',
+        }}
+      >
+        <h1>Examix</h1>
+        <div css={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+          <MainButton onClick={() => navigate('/login')} variant="contained">
+            Login as teacher
+          </MainButton>
+          <MainButton onClick={() => navigate('/join')} variant="contained">
+            Join the test
+          </MainButton>
+        </div>
       </div>
     </Layout>
   );
