7627df7a80409eab73b9311582cae6f1868ebd0a
macsbook <maks.naumovich@gmail.com>
2024-03-15 15:56:01 +0100

    change onclick routing to href on mainbutton in StartPage.tsx, add enum Routes

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,32 +1,29 @@
-import { useNavigate } from 'react-router-dom';
 import MainButton from '../components/MainButton';
 import Layout from '../Layout';
+import Routes from '../Routes';
 
-const StartPage: React.FC = () => {
-  const navigate = useNavigate();
-  return (
-    <Layout>
-      <div
-        css={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: '25px',
-          height: '300px',
-        }}
-      >
-        <h1>Examix</h1>
-        <div css={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
-          <MainButton onClick={() => navigate('/login')} variant="contained">
-            Login as teacher
-          </MainButton>
-          <MainButton onClick={() => navigate('/join')} variant="contained">
-            Join the test
-          </MainButton>
-        </div>
+const StartPage: React.FC = () => (
+  <Layout>
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '25px',
+        height: '300px',
+      }}
+    >
+      <h1>Examix</h1>
+      <div css={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+        <MainButton href={Routes.LOGIN_PAGE} variant="contained">
+          Login as teacher
+        </MainButton>
+        <MainButton href={Routes.JOIN_PAGE} variant="contained">
+          Join the test
+        </MainButton>
       </div>
-    </Layout>
-  );
-};
+    </div>
+  </Layout>
+);
 
 export default StartPage;
