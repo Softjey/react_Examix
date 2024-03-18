784c710050874aa7fa8cf859e92905a8732873e4
macsbook <maks.naumovich@gmail.com>
2024-03-18 17:39:19 +0100

    refactor login page, reuse login page component, refactor and reuse loginheader, use mui components insead of divs

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,64 @@
+import { Stack, TextField } from '@mui/material';
+import Layout from '../../Layout';
+import MainButton from '../../components/MainButton';
+import Header from '../../components/Header';
+import { Role, useLoginPage } from './useLoginPage';
+
+interface Props {
+  role: Role;
+}
+
+// eslint-disable-next-line react/prop-types
+const LoginPage: React.FC<Props> = ({ role }) => {
+  const { fields, inputProps } = useLoginPage(role);
+  return (
+    <Layout>
+      <div
+        css={{
+          height: '300px',
+          gap: '20px',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Header />
+        <form
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '20px',
+          }}
+        >
+          <Stack width="300px" direction="column" spacing={2}>
+            <TextField
+              label={fields.input1Label}
+              placeholder={fields.input1Placeholder}
+              variant="outlined"
+              inputProps={inputProps.input1}
+              fullWidth
+            />
+            <TextField
+              label={fields.input2Label}
+              placeholder={fields.input2Placeholder}
+              type={fields.input2Type}
+              variant="outlined"
+              inputProps={inputProps.input2}
+              fullWidth
+              sx={{
+                '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button': {
+                  display: 'none',
+                },
+                '& input[type=number]': {
+                  MozAppearance: 'textfield',
+                },
+              }}
+            />
+          </Stack>
+          <MainButton variant="contained">{fields.buttonText}</MainButton>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+export default LoginPage;
---
+++ src/pages/LoginPage/useLoginPage.ts
@@ -0,0 +1,67 @@
+import { InputBaseComponentProps } from '@mui/material';
+
+export type Role = 'teacher' | 'student';
+
+interface Fields {
+  input1Placeholder: string;
+  input2Placeholder: string;
+  input1Label: string;
+  input2Label: string;
+  input2Type: React.HTMLInputTypeAttribute;
+  buttonText: string;
+}
+
+interface InputProps {
+  input1: InputBaseComponentProps;
+  input2: InputBaseComponentProps;
+}
+
+function getInputProps(role: Role): InputProps {
+  const defaultInputProps = {
+    input1: {},
+    input2: {},
+  };
+
+  switch (role) {
+    case 'teacher':
+      return defaultInputProps;
+    case 'student':
+      return { ...defaultInputProps, input2: { maxLength: 6 } };
+    default:
+      throw new Error('Role is unhandled');
+  }
+}
+
+function getFields(role: Role): Fields {
+  switch (role) {
+    case 'teacher':
+      return {
+        input1Placeholder: 'Enter email',
+        input2Placeholder: 'Enter password',
+        input1Label: 'Email',
+        input2Label: 'Password',
+        input2Type: 'password',
+        buttonText: 'Login',
+      };
+    case 'student':
+      return {
+        input1Placeholder: 'Enter name',
+        input2Placeholder: 'Enter code',
+        input1Label: 'Name',
+        input2Label: 'Code',
+        input2Type: 'number',
+        buttonText: 'Join the test',
+      };
+    default:
+      throw new Error('Role is unhandled');
+  }
+}
+
+export function useLoginPage(role: Role) {
+  const fields = getFields(role);
+  const inputProps = getInputProps(role);
+  return {
+    fields,
+    inputProps,
+  };
+}
