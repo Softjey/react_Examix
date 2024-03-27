e093e9c5677d644943a5830a0931e9aaaafdb642
Softjey <misivsvatoslav@gmail.com>
2024-03-27 14:40:35 +0100

    add server folder, separate server and client, add scripts for full project

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: '**/tsconfig.json',
   },
   plugins: ['react-refresh', '@emotion'],
   rules: {
