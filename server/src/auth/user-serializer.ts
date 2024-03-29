14bdc6cf85a28b5674d93f9483ae5724187e9552
Softjey <misivsvatoslav@gmail.com>
2024-03-29 14:51:51 +0100

    delete users service dependencie from user serializer

--- server/src/auth/user-serializer.ts
+++ server/src/auth/user-serializer.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PassportSerializer } from '@nestjs/passport';
 import { User } from '@prisma/client';
-import { UsersService } from 'src/users/users.service';
 import { SessionUserDto } from './dto/session-user.dto';
 
 @Injectable()
 export class UserSerializer extends PassportSerializer {
-  constructor(private readonly usersService: UsersService) {
-    super();
-  }
-
   serializeUser(user: User, done: (err: any, sessionUser?: SessionUserDto) => void) {
     done(null, { id: user.id });
   }
