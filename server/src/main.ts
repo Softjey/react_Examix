f11405ea041027ab02a78c9b888e74f9c6874d4d
Softjey <misivsvatoslav@gmail.com>
2024-03-29 16:21:39 +0100

    integrate swagger documentation in server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from './config';
+import { enableApiDocs } from './utlis/documentaion';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  enableApiDocs('api/docs', app);
 
   await app.listen(config.PORT, () => {
     console.log(`Server is running on http://localhost:${config.PORT}`);
--- server/src/users/dtos/create-user.dto.ts
+++ server/src/users/dtos/create-user.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { $Enums, User } from '@prisma/client';
+import { IsEmail, IsEnum, IsString } from 'class-validator';
 
 export class CreateUserDto
   implements Pick<User, 'email' | 'password' | 'name'>, Partial<Pick<User, 'role'>>
 {
+  @IsEmail()
   email: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
   password: string;
+
+  @ApiProperty({ enumName: 'Role', enum: $Enums.Role })
+  @IsEnum($Enums.Role)
   role?: $Enums.Role;
 }
