f3df6e169200a9e01273fa92ce0de17100fdc337
Softjey <misivsvatoslav@gmail.com>
2024-03-31 15:33:17 +0200

    add gloabal validation pipes and global server exception filter

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from './config';
 import { enableApiDocs } from './utils/documentation';
+import { ValidationPipe } from '@nestjs/common';
+import { ServerExceptionFilter } from './utils/exceptions/server-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
 
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new ServerExceptionFilter(httpAdapter));
   app.enableCors();
   enableApiDocs('api/docs', app);
 
