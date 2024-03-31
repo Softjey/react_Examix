9da4a280838c51a5e99683d69589b6f02c2f10cf
macsbook <maks.naumovich@gmail.com>
2024-03-31 21:55:27 +0300

    fix problem with progress transition reseting

--- client/src/components/QuizCard.tsx
+++ client/src/components/QuizCard.tsx
@@ -1,21 +1,30 @@
 /* eslint-disable object-curly-newline */
 import { Button, CardActions, CardHeader, ButtonGroup, LinearProgress } from '@mui/material';
 import Card from '@mui/material/Card';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Question, Answer } from '../temp/questions';
-import useCountDown from '../hoooks/useCountdown';
+import useCountDown from '../hooks/useCountdown';
 
 interface Props {
   question: Question;
-  progress: number;
+  quizProgress: number;
   setAnswers: React.Dispatch<React.SetStateAction<Answer[]>>;
   nextQuestion: () => void;
 }
 
-const QuizCard: React.FC<Props> = ({ question, progress, nextQuestion, setAnswers }) => {
+const QuizCard: React.FC<Props> = ({ question, quizProgress, nextQuestion, setAnswers }) => {
   const { msLeft, start } = useCountDown();
 
+  const fixProgressAnimationStyles = {
+    '&[aria-valuenow="100"]': {
+      '& > .progressBarInner': {
+        transition: 'none',
+      },
+    },
+  };
+
   const time = 5000;
+  const progress = (msLeft / time) * 100;
 
   function onButtonClick(variant: string) {
     setAnswers((prev) => [...prev, { questionId: question.id, answer: variant }]);
@@ -30,27 +39,36 @@ const QuizCard: React.FC<Props> = ({ question, progress, nextQuestion, setAnswer
 
   useEffect(() => {
     if (msLeft === 0) {
-      setAnswers((prev) => [...prev, { questionId: question.id, answer: 'unset' }]);
-      start(time);
-      nextQuestion();
+      onButtonClick('unset');
     }
   });
 
   return (
     <Card elevation={5} sx={{ width: '500px', padding: '20px' }}>
       <span>{Math.ceil(msLeft / 1000)}</span>
-      <LinearProgress variant="determinate" value={(msLeft / time) * 100} />
+      <LinearProgress
+        sx={fixProgressAnimationStyles}
+        classes={{ bar: 'progressBarInner' }}
+        variant="determinate"
+        value={progress}
+      />
       <CardHeader title={question.title} />
       <CardActions>
         <ButtonGroup fullWidth orientation="vertical">
-          {question.variants.map((variant) => (
-            <Button onClick={() => onButtonClick(variant)} sx={{ height: '60px' }} size="large">
+          {question.variants.map((variant, i) => (
+            <Button
+              // eslint-disable-next-line react/no-array-index-key
+              key={i}
+              onClick={() => onButtonClick(variant)}
+              sx={{ height: '60px' }}
+              size="large"
+            >
               {variant}
             </Button>
           ))}
         </ButtonGroup>
       </CardActions>
-      <LinearProgress variant="determinate" value={progress} />
+      <LinearProgress variant="determinate" value={quizProgress} />
     </Card>
   );
 };
--- client/src/pages/QuizPage.tsx
+++ client/src/pages/QuizPage.tsx
@@ -1,22 +1,30 @@
+/* eslint-disable no-nested-ternary */
 import { useState } from 'react';
 import Layout from '../Layout';
 import { Answer, newQuestions as questions } from '../temp/questions';
 import QuizCard from '../components/QuizCard';
 import Result from '../components/Result';
+import MainButton from '../components/UI/buttons/MainButton';
 
 const QuizPage: React.FC = () => {
   const [questionIndex, setQuestionIndex] = useState(0);
   // state with saved answers to compare on server
   const [answers, setAnswers] = useState<Answer[]>([]);
-  const progress = Math.round((questionIndex / questions.length) * 100);
+  const quizProgress = Math.round((questionIndex / questions.length) * 100);
+  const [isStarted, setIsStarted] = useState(false);
+
   return (
     <Layout>
-      {questionIndex < questions.length ? (
+      {!isStarted ? (
+        <MainButton variant="contained" onClick={() => setIsStarted(true)}>
+          Start
+        </MainButton>
+      ) : questionIndex < questions.length ? (
         <QuizCard
           nextQuestion={() => setQuestionIndex(questionIndex + 1)}
           setAnswers={setAnswers}
           question={questions[questionIndex]}
-          progress={progress}
+          quizProgress={quizProgress}
         />
       ) : (
         // this is the test fragment, it should be replaced with results page
