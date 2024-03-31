1d65ca81439ee497e9ab758f3dee1b38874d5457
Softjey <misivsvatoslav@gmail.com>
2024-03-31 15:24:33 +0200

    make User decorator more typed

--- server/src/modules/auth/decorators/user.decorator.ts
+++ server/src/modules/auth/decorators/user.decorator.ts
@@ -3,10 +3,11 @@ import { User as UserModel } from '@prisma/client';
 
 export const User = createParamDecorator((property: keyof UserModel, context: ExecutionContext) => {
   const request = context.switchToHttp().getRequest();
+  const user = request.user as UserModel;
 
   if (property) {
     return request.user[property];
   }
 
-  return request.user as UserModel;
+  return user;
 });
