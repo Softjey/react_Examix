2b98994f84f84f0d5c1f96b1eea571c2b2b1a790
Softjey <misivsvatoslav@gmail.com>
2024-04-02 12:36:25 +0200

    reduce data amount in author

--- server/src/modules/authors/author.entity.ts
+++ server/src/modules/authors/author.entity.ts
@@ -2,10 +2,12 @@ import { User } from '@prisma/client';
 import { Socket } from 'socket.io';
 
 export class Author {
+  readonly userId: User['id'];
+  readonly authorToken: string;
   clientId?: Socket['id'];
 
-  constructor(
-    readonly user: User,
-    readonly authorToken: string,
-  ) {}
+  constructor(user: User, authorToken: string) {
+    this.authorToken = authorToken;
+    this.userId = user.id;
+  }
 }
