89987efa601cbd32f89995008973c4d9c44ae05e
Softjey <misivsvatoslav@gmail.com>
2024-04-03 14:38:14 +0200

    add ExamsGateway to providers

--- server/src/modules/exams/exams.module.ts
+++ server/src/modules/exams/exams.module.ts
@@ -4,10 +4,11 @@ import { TestsModule } from '../tests/tests.module';
 import { UniqueIdModule } from '../unique-id/unique-id.module';
 import { RedisModule } from '../redis/redis.module';
 import { ExamsController } from './exams.controller';
+import { ExamsGateway } from './exams.gateway';
 
 @Module({
   imports: [TestsModule, UniqueIdModule, RedisModule],
-  providers: [ExamsService],
+  providers: [ExamsService, ExamsGateway],
   controllers: [ExamsController],
   exports: [ExamsService],
 })
