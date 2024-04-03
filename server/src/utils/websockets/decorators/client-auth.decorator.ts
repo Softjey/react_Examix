3b291d6fec16691aedc3396931ff1e65d18613a5
Softjey <misivsvatoslav@gmail.com>
2024-04-03 03:52:38 +0200

    fix ClientAuth decorator types

--- server/src/utils/websockets/decorators/client-auth.decorator.ts
+++ server/src/utils/websockets/decorators/client-auth.decorator.ts
@@ -1,11 +1,11 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-import { ClientAuthorAuthDto } from '../../../modules/rooms/dtos/client-auth.dto';
 import { Socket } from 'socket.io';
 
-export const ClientAuth = createParamDecorator(
-  (property: keyof ClientAuthorAuthDto, context: ExecutionContext) => {
-    const { auth } = context.switchToWs().getClient<Socket>().handshake;
+export const ClientAuth = createParamDecorator(function <T extends Record<string, any>>(
+  property: keyof T,
+  context: ExecutionContext,
+) {
+  const auth = context.switchToWs().getClient<Socket>().handshake.auth as T;
 
-    return property ? auth[property] : auth;
-  },
-);
+  return property ? auth[property] : auth;
+});
