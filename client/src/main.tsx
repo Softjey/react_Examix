8efb7232d193d47b793a572e3998ac3735744540
Softjey <misivsvatoslav@gmail.com>
2024-04-04 08:08:16 +0200

    connect query client provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createHashRouter as createRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import StartPage from './pages/StartPage';
 import NotFoundPage from './pages/NotFoundPage';
@@ -8,6 +9,7 @@ import Routes from './constants/Router/Routes';
 import TestPage from './dev/TestPage';
 import QuizPage from './dev/QuizPage';
 
+const queryClient = new QueryClient();
 const router = createRouter([
   {
     path: Routes.START_PAGE,
@@ -35,4 +37,8 @@ const router = createRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
+);
