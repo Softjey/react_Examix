d4b7e15e3bfe8f011b9058066e22bc7349a4ba61
Softjey <misivsvatoslav@gmail.com>
2024-04-05 17:17:11 +0200

    fix user creating exceptions, made password length validation

--- server/src/modules/users/dtos/create-user.dto.ts
+++ server/src/modules/users/dtos/create-user.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { $Enums, User } from '@prisma/client';
-import { IsEmail, IsEnum, IsString } from 'class-validator';
+// eslint-disable-next-line max-len
+import { IsEmail, IsEnum, IsNotEmpty, IsString, IsStrongPassword, MinLength } from 'class-validator'; // prettier-ignore
 
 export class CreateUserDto
   implements Pick<User, 'email' | 'password' | 'name'>, Partial<Pick<User, 'role'>>
@@ -9,9 +10,15 @@ export class CreateUserDto
   email: string;
 
   @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   name: string;
 
   @IsString()
+  @IsStrongPassword(
+    { minLength: 8, minLowercase: 0, minSymbols: 0, minNumbers: 0, minUppercase: 0 },
+    { message: 'Password have to be at least 8 characters long' },
+  )
   password: string;
 
   @ApiProperty({ enumName: 'Role', enum: $Enums.Role, required: false })
--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -1,15 +1,26 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UseSessionGuard } from 'src/modules/auth/decorators/session-guard.decorator';
 
 @Controller('users')
+@UseSessionGuard()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @UseSessionGuard()
-  create(@Body(new ValidationPipe()) body: CreateUserDto) {
-    this.usersService.create(body);
+  async create(@Body() body: CreateUserDto) {
+    const user = await this.usersService.getByEmail(body.email);
+
+    if (user) {
+      throw new BadRequestException('User with this email already exists');
+    }
+
+    const newUser = await this.usersService.create(body);
+
+    return {
+      message: 'User created successfully',
+      newUser: this.usersService.normalize(newUser),
+    };
   }
 }
