c88460182370078b4ec7373f394cc83fabd25e43
Softjey <misivsvatoslav@gmail.com>
2024-04-10 20:54:15 +0200

    implement logout functionality

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { HttpCode, HttpStatus, InternalServerErrorException } from '@nestjs/common';
 import { UsersService } from 'src/modules/users/users.service';
 import { LoginGuard } from './guards/login.guard';
 import { User } from './decorators/user.decorator';
@@ -6,6 +7,7 @@ import { User as UserI } from '@prisma/client';
 import { LoginDto } from './dto/login.dto';
 import { ApiBody } from '@nestjs/swagger';
 import { UseSessionGuard } from './decorators/session-guard.decorator';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -28,6 +30,20 @@ export class AuthController {
     return {
       message: 'You are authenticated!',
       user: this.usersService.normalize(user),
+    };
+  }
+
+  @Get('logout')
+  @UseSessionGuard()
+  async logout(@Req() request: Request) {
+    request.logout((error: Error) => {
+      if (error) {
+        throw new InternalServerErrorException('Failed to logout. Please try again.');
+      }
+    });
+
+    return {
+      message: 'Logout successful. Goodbye!',
     };
   }
 }
