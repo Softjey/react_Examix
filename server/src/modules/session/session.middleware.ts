d6d278e53d73913ab5dc4a4ac58c77d3e80dcb24
Softjey <misivsvatoslav@gmail.com>
2024-04-10 20:25:40 +0200

    connect redis to sessions

---
+++ server/src/modules/session/session.middleware.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
+import { Redis } from 'ioredis';
+import * as session from 'express-session';
+import RedisStore from 'connect-redis';
+import config from 'src/config';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class SessionMiddleware implements NestMiddleware {
+  constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    session({
+      secret: config.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      rolling: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * config.SESSION_MAX_AGE,
+      },
+      store: new RedisStore({
+        client: this.redisClient,
+        prefix: 'session:',
+        ttl: 60 * 60 * 24 * config.SESSION_MAX_AGE,
+      }),
+    })(req, res, next);
+  }
+}
