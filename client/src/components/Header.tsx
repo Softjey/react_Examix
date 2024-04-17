e7c22e5852b511841cd614a5289b74ffe5959010
macsbook <maks.naumovich@gmail.com>
2024-04-17 10:45:36 +0200

    use Typography mui component instead of h1 in all files, delete uneccessery files

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { CSSObject } from '@emotion/react';
+import { Typography } from '@mui/material';
 import BackButton from './UI/buttons/BackButton';
 
 interface Props {
@@ -8,7 +9,9 @@ interface Props {
 
 const Header: React.FC<Props> = ({ disableBackBtn, style }) => (
   <header css={{ position: 'relative', ...style }}>
-    <h1>Examix</h1>
+    <Typography fontSize={70} variant="h1" fontWeight={700}>
+      Examix
+    </Typography>
     {!disableBackBtn && (
       <BackButton
         style={{ position: 'absolute', transform: 'translateY(-50%)', top: '50%', left: '-95px' }}
--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -1,11 +1,16 @@
+import { Typography } from '@mui/material';
 import StartLayout from '../components/StartLayout';
 import Button from '../components/UI/buttons/Button';
 import Routes from '../services/Router/Routes';
 
 const NotFoundPage: React.FC = () => (
   <StartLayout header={false}>
-    <h1 css={{ fontSize: '6.25rem' }}>404</h1>
-    <h2 css={{ marginBottom: '20px' }}>Page Not Found</h2>
+    <Typography fontWeight={700} variant="h1">
+      404
+    </Typography>
+    <Typography fontWeight={600} variant="h2">
+      Page Not Found
+    </Typography>
     <Button to={Routes.START_PAGE} variant="contained" size="large">
       Home
     </Button>
