473361f233f3cb877a4e66f2d31806b06a2159ae
macsbook <maks.naumovich@gmail.com>
2024-04-22 14:21:13 +0200

    modify test info card

--- client/src/dev/WaitingPage.tsx
+++ client/src/dev/WaitingPage.tsx
@@ -9,28 +9,6 @@ import useStudentExamSocket from './useStudentExamSocket';
 import { TestInfo } from './questions';
 import { snakeCaseToNormal } from './formatter';
 
-/* const icons: Icons = {
-  MATH: <FunctionsRounded />,
-  ENGLISH: <TranslateOutlined />,
-  SCIENCE: <BiotechOutlined />,
-  HISTORY: <AccountBalanceOutlined />,
-  GEOGRAPHY: <Language />,
-  COMPUTER_SCIENCE: <Computer fontSize="large" />,
-  PHYSICS: <BiotechOutlined />,
-  CHEMISTRY: <ScienceOutlined />,
-  BIOLOGY: <SpaRounded />,
-  ECONOMICS: <AttachMoney />,
-  CIVICS: <Gavel />,
-  GOVERNMENT: <Gavel />,
-  LITERATURE: <MenuBook />,
-  ART: <Brush />,
-  MUSIC: <MusicNoteOutlined />,
-  PHYSICAL_EDUCATION: <FitnessCenter />,
-  HEALTH: <HealthAndSafety />,
-  SPANISH: <TranslateOutlined />,
-  FRENCH: <TranslateOutlined />,
-  GERMAN: <TranslateOutlined />,
-}; */
 const WaitingPage: React.FC = () => {
   const [testInfo, setTestInfo] = useState<TestInfo | null>(null);
   const { isLoading } = useStudentExamSocket((newSocket: Socket) => {
@@ -38,7 +16,7 @@ const WaitingPage: React.FC = () => {
   });
   return (
     <StartLayout innerStyle={{ gap: '100px' }} header={false}>
-      {isLoading && (
+      {!isLoading && (
         <>
           <Typography sx={{ fontWeight: '500' }} variant="h2">
             <DottedText text="Waiting for the exam to start" />
@@ -64,9 +42,7 @@ const WaitingPage: React.FC = () => {
                 <div className="right">
                   <Typography variant="h6">{testInfo.name}</Typography>
                   <Typography sx={{ maxWidth: '300px' }} variant="body1" color="text.secondary">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque corrupti, autem
-                    temporibus, dolor fugit blanditiis eius repellat reprehenderit quis aliquid
-                    voluptas nihil iure, unde dolores? Et id pariatur vel consequatur!
+                    {testInfo.description}
                   </Typography>
                 </div>
                 <div
@@ -94,19 +70,24 @@ const WaitingPage: React.FC = () => {
                 <div
                   style={{
                     position: 'absolute',
-                    top: '20px',
-                    right: '20px',
+                    top: '30px',
+                    right: '30px',
                     display: 'flex',
                     flexFlow: 'column wrap',
                     maxHeight: '80px',
-                    width: '160px',
+                    width: '140px',
                   }}
                 >
                   <Typography variant="subtitle1">Subject:</Typography>
                   <Typography sx={{ maxWidth: '80px' }} variant="body1" color="text.secondary">
-                    {snakeCaseToNormal(testInfo.subject)}Computer Science
+                    {snakeCaseToNormal(testInfo.subject)}
                   </Typography>
-                  <img height="80px" src={`../icons/${testInfo.subject}.svg`} alt="icon" />
+
+                  <img
+                    height="60px"
+                    src={`/react_Examix/src/icons/${testInfo.subject}.svg`}
+                    alt="icon"
+                  />
                 </div>
               </CardContent>
             </Card>
