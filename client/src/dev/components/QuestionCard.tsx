bb47a91edd934d3ac7abfbfd3db94398e3b52f2d
macsbook <maks.naumovich@gmail.com>
2024-05-19 22:02:44 +0200

    add question type select, maxscore select

--- client/src/dev/components/QuestionCard.tsx
+++ client/src/dev/components/QuestionCard.tsx
@@ -1,42 +1,53 @@
 import { useState } from 'react';
-import {
-  Box,
-  Card,
-  CardContent,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Paper,
-  Select,
-  SelectChangeEvent,
-} from '@mui/material';
+import { Box, Card, CardContent, MenuItem, Paper, TextField } from '@mui/material';
 import { QuestionType } from '../questions';
+import { snakeCaseToNormal } from '../formatter';
 
 interface Props {}
 const QuestionCard: React.FC<Props> = () => {
-  const [age, setAge] = useState('');
+  const [questionType, setQuestionType] = useState<QuestionType>(QuestionType.SINGLE_CHOICE);
+  const [maxScore, setMaxScore] = useState<number>(1);
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value as string);
-  };
+  const points = Array.from({ length: 20 }, (_, i) => i + 1);
+
   return (
     <Card component={Paper} elevation={2}>
       <CardContent>
-        <Box display="flex" justifyContent="space-between">
-          <FormControl fullWidth>
-            <InputLabel id="question-type-label">Question type</InputLabel>
-            <Select
-              labelId="question-type-label"
-              id="demo-simple-select"
-              value={age}
-              label="Question type"
-              onChange={handleChange}
-            >
-              {(Object.keys(QuestionType) as Array<keyof typeof QuestionType>).map((type) => (
-                <MenuItem value={type}>{type}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+        <Box display="flex" gap={3}>
+          <TextField
+            value={questionType}
+            onChange={(e) => setQuestionType(e.target.value as QuestionType)}
+            select
+            label="Question type"
+          >
+            {(Object.keys(QuestionType) as Array<keyof typeof QuestionType>).map((type) => (
+              <MenuItem key={type} value={type}>
+                {snakeCaseToNormal(type)}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField label="time limit" disabled />
+          <TextField
+            select
+            value={maxScore}
+            onChange={(e) => setMaxScore(Number(e.target.value))}
+            label="Max score"
+            SelectProps={{
+              MenuProps: {
+                PaperProps: {
+                  style: {
+                    maxHeight: 200,
+                  },
+                },
+              },
+            }}
+          >
+            {points.map((point) => (
+              <MenuItem key={point} value={point}>
+                {point} {point === 1 ? 'point' : 'points'}
+              </MenuItem>
+            ))}
+          </TextField>
         </Box>
       </CardContent>
     </Card>
