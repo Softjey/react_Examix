3e223afac67662efad3b440cfc684d982c9df954
Softjey <misivsvatoslav@gmail.com>
2024-05-20 18:13:23 +0200

    update imports after common folder update

--- client/src/components/layouts/StartLayout.tsx
+++ client/src/components/layouts/StartLayout.tsx
@@ -2,7 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import React from 'react';
-import Header from '../Header';
+import Header from '../common/Header';
 import { center, columnCenter } from '../../styles/flex';
 import { ComponentProps } from '../../types/ComponentProps';
 
