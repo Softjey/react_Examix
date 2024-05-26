57e8b3542e26df476b1531951e3befb65ad0f29b
Softjey <misivsvatoslav@gmail.com>
2024-05-26 12:53:18 +0200

    add new functionality and chip variant for SubjectItem

---
+++ client/src/components/UI/SubjectItem/SubjectItemContent.tsx
@@ -0,0 +1,44 @@
+import { Typography, TypographyProps } from '@mui/material';
+import React from 'react';
+import { textEllipsis } from '../../../styles/text';
+import getSubjectImgPath from '../../../utils/getSubjectImgPath';
+import underscoreToUpperToSentence from '../../../utils/underscoreToUpperToSentence';
+import Subject from '../../../types/api/enums/Subject';
+import { Nullable } from '../../../types/utils/Nullable';
+import getIconSizeByTypographyVariant from '../../../utils/getIconSizeByTypographyVariant';
+
+interface Props {
+  subject?: Nullable<Subject>;
+  endText?: string;
+  typographyProps?: Omit<TypographyProps, 'variant'>;
+  textVariant?: TypographyProps['variant'];
+}
+
+const SubjectItemContent: React.FC<Props> = ({
+  subject,
+  endText,
+  textVariant = 'caption',
+  typographyProps,
+}) => {
+  const iconSize = getIconSizeByTypographyVariant(textVariant);
+
+  return (
+    <>
+      {subject && (
+        <img src={getSubjectImgPath(subject)} alt={subject} width={iconSize} height={iconSize} />
+      )}
+
+      <Typography
+        component="span"
+        variant={textVariant}
+        {...typographyProps}
+        sx={{ maxWidth: '100%', lineHeight: 'normal', ...textEllipsis, ...typographyProps?.sx }}
+      >
+        {subject ? underscoreToUpperToSentence(subject) : 'No subject'}
+        {endText ? `. ${endText}` : ''}
+      </Typography>
+    </>
+  );
+};
+
+export default SubjectItemContent;
