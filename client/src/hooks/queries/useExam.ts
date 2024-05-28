2567817a67948adc368c15fc99113610f8a833a4
Softjey <misivsvatoslav@gmail.com>
2024-05-28 22:20:04 +0200

    make nullable id in useExam, add roundToFixedLength util function

--- client/src/hooks/queries/useExam.ts
+++ client/src/hooks/queries/useExam.ts
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { Exam } from '../../types/api/entities/exam';
 import ApiClient from '../../services/Api/ApiClient';
+import { Nullable } from '../../types/utils/Nullable';
 
-export default function useExam(id?: Exam['id']) {
+export default function useExam(id: Nullable<Exam['id']>) {
   const { data, ...rest } = useQuery({
     queryKey: ['exam', id],
     queryFn: async () => {
