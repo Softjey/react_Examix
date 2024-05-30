8b7da7f21637e5c95495c56eff1aed550ad9ec66
Softjey <misivsvatoslav@gmail.com>
2024-05-30 22:50:01 +0200

    update TestsLibaryPage to use updated components

--- client/src/pages/TestsLibraryPage.tsx
+++ client/src/pages/TestsLibraryPage.tsx
@@ -1,25 +1,17 @@
 import * as React from 'react';
 import useTests from '../hooks/queries/useTests';
 import TestsList from '../components/common/TestsList';
-import LoadingPage from './LoadingPage';
-import { columnCenter } from '../styles/flex';
 import SearchAndSelect from '../components/common/SearchAndSelect';
 import HomeLayout, { Props as HomeLayoutProps } from '../components/layouts/HomeLayout';
 
 interface Props extends HomeLayoutProps {}
-const TestsLibraryPage: React.FC<Props> = ({ contentSx, ...rest }) => {
-  const { tests, isLoading, isError } = useTests({ limit: 2 });
+const TestsLibraryPage: React.FC<Props> = ({ ...rest }) => {
+  const { tests, isLoading, error } = useTests({ limit: 10 });
 
-  if (isLoading || !tests) {
-    return <LoadingPage />;
-  }
-  if (isError || !tests) {
-    return <h1>error</h1>;
-  }
   return (
-    <HomeLayout contentSx={{ ...columnCenter, ...contentSx }} {...rest}>
+    <HomeLayout centered {...rest}>
       <SearchAndSelect />
-      <TestsList tests={tests} />
+      <TestsList tests={tests} isLoading={isLoading} error={error?.message} sx={{ mt: 5 }} />
     </HomeLayout>
   );
 };
