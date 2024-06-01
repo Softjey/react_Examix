9cf27bdca25c12d0b3eea15e28651ce2b171cce4
Softjey <misivsvatoslav@gmail.com>
2024-06-01 16:42:23 +0200

    add message to students list, when students length is 0, add start exam button to ongoing page

--- client/src/pages/OngoingExamPage.tsx
+++ client/src/pages/OngoingExamPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Navigate } from 'react-router';
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import HomeLayout, { Props as HomeLayoutProps } from '../components/layouts/HomeLayout';
 import teacherExamStore from '../store/ExamStore/TeacherExamStore';
 import Routes from '../services/Router/Routes';
@@ -9,6 +9,7 @@ import BaseTestInfo from '../components/common/BaseTestInfo';
 import QuestionsList from '../components/common/QuestionsList';
 import LoadingPage from './LoadingPage';
 import StudentsList from '../components/common/StudentsList';
+import Button from '../components/UI/buttons/Button';
 
 interface Props extends HomeLayoutProps {}
 
@@ -28,9 +29,15 @@ const OngoingExamPage: React.FC<Props> = observer(({ ...rest }) => {
       <BaseTestInfo
         test={test}
         action={
-          <Typography align="center" variant="h4" color={(theme) => theme.palette.secondary.main}>
-            {teacherExamStore.examCode}
-          </Typography>
+          <Stack direction="row" justifyContent="space-around">
+            <Typography align="center" variant="h4" color={(theme) => theme.palette.secondary.dark}>
+              {teacherExamStore.examCode}
+            </Typography>
+
+            <Button variant="contained" color="secondary">
+              Start exam
+            </Button>
+          </Stack>
         }
       />
 
