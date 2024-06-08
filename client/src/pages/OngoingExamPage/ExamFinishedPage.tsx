6fc03d8f6f41085ed23afd750c701b9e83d31d8c
Sasza Nochovna <nochovna.sasha@gmail.com>
2024-06-08 18:21:40 +0200

    implement exam finished page

--- client/src/pages/OngoingExamPage/ExamFinishedPage.tsx
+++ client/src/pages/OngoingExamPage/ExamFinishedPage.tsx
@@ -1,36 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Typography } from '@mui/material';
-import { Navigate, useNavigate } from 'react-router';
+import { Stack } from '@mui/material';
+import { useNavigate } from 'react-router';
 import QuizLayout from '../../components/layouts/QuizLayout';
-import studentExamStore from '../../store/ExamStore/StudentExamStore';
-import Button from '../../components/UI/buttons/Button';
+// import studentExamStore from '../../store/ExamStore/StudentExamStore';
 import Routes from '../../services/Router/Routes';
+import getRandom from '../../utils/getRandom';
+import Button from '../../components/UI/buttons/Button';
 
 interface Props {}
 
 const ExamFinishedPage: React.FC<Props> = observer(() => {
-  const { exam } = studentExamStore;
+  const [randomNum] = useState(() => getRandom(1, 19));
   const navigate = useNavigate();
 
-  if (!exam) {
-    return <Navigate to={Routes.START} />;
-  }
-
   const handleGoHomeClick = () => {
-    studentExamStore.resetExam();
+    // studentExamStore.resetExam();
     navigate(Routes.START);
   };
 
   return (
     <QuizLayout centeredProps={{ sx: { maxWidth: 400 } }}>
-      <Typography variant="h4" paragraph>
-        Exam Finished. Good job!
-      </Typography>
+      <Stack direction="column" spacing={3} alignItems="center">
+        <Typography variant="h3" paragraph>
+          Test Completed!
+        </Typography>
+        <Typography
+          variant="body2"
+          color={(t) => t.palette.text.secondary}
+          paragraph
+          sx={{ textAlign: 'center' }}
+        >
+          Hey, great job on the exam! Time to chill and recharge. You deserve a break.
+        </Typography>
 
-      <Button onClick={handleGoHomeClick} variant="contained">
-        Go home
-      </Button>
+        <img
+          src={`public/gifs/exam-finished/${randomNum}.gif`}
+          alt="Test completed. Good job!"
+          style={{ height: '300px', width: 'auto' }}
+        />
+
+        <Button size="large" onClick={handleGoHomeClick} variant="contained">
+          Return to Home
+        </Button>
+      </Stack>
     </QuizLayout>
   );
 });
