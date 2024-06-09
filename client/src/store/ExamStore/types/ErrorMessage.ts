b57e3a3015374f26450875038bd013b8402593a2
Softjey <misivsvatoslav@gmail.com>
2024-06-09 21:15:24 +0200

    add messages to exam deletetion logic

--- client/src/store/ExamStore/types/ErrorMessage.ts
+++ client/src/store/ExamStore/types/ErrorMessage.ts
@@ -3,6 +3,7 @@ const enum ErrorMessage {
   STUDENT_ID_INCORRECT = 'Student not found. Please, check the student id',
   INVALID_STUDENT_TOKEN = 'Student token is required or invalid, if you provided studentId ',
   NOT_AUTHOR = 'You are not an author of this exam',
+  EXAM_WAS_DELETED = 'The exam was deleted by the author or it has been deleted due to inactivity',
 }
 
 export default ErrorMessage;
--- client/src/store/ExamStore/types/Message.ts
+++ client/src/store/ExamStore/types/Message.ts
@@ -2,19 +2,19 @@ enum Message {
   CONNECTED = 'connected',
   DISCONNECT = 'disconnect',
   RECONNECTED = 'reconnected',
+  EXCEPTION = 'exception',
 
   STUDENT_JOINED = 'student-joined',
   STUDENT_RECONNECTED = 'student-reconnected',
   STUDENT_DISCONNECTED = 'student-disconnected',
 
-  EXCEPTION = 'exception',
-
+  QUESTION = 'question',
   RESULTS = 'results',
 
   EXAM_STARTED = 'exam-started',
-  QUESTION = 'question',
-
   EXAM_FINISHED = 'exam-finished',
+  EXAM_DELETED = 'exam-deleted',
+
   STUDENT_KICKED = 'student-kicked',
 }
 
