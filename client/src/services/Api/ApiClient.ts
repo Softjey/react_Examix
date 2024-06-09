0384615b3e1ab35f20de1b4bb193581f208d2e4d
Sasza Nochovna <nochovna.sasha@gmail.com>
2024-06-09 18:48:19 +0200

    add update me method in api client

--- client/src/services/Api/ApiClient.ts
+++ client/src/services/Api/ApiClient.ts
@@ -14,6 +14,7 @@ import ApiError from './ApiError';
 import { CreateQuestionDto, CreateQuestionsResponse } from './types/create-questions';
 import { CreateTestDto, CreateTestResponse } from './types/create-test';
 import { QuestionsParams, QuestionsResponse } from './types/questions';
+import { UpdateMeDto, UpdateMeResponse } from './types/update-me';
 
 const axios = axiosCLient.create({
   baseURL: import.meta.env.VITE_SERVER_HTTP_URL,
@@ -108,6 +109,12 @@ export class RawApiClient {
     const { data } = await axios.get<DetailedExam>(`/exams/${id}`);
 
     return data;
+  }
+
+  static async updateMe(dto: UpdateMeDto) {
+    const { data } = await axios.patch<UpdateMeResponse>('/users/me', dto);
+
+    return data.user;
   }
 
   static async getQuestions(params: QuestionsParams = {}) {
