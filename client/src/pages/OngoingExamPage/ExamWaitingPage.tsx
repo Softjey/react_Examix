56e1ad38680757104ba05a155f6fe2d445dc7ba1
Softjey <misivsvatoslav@gmail.com>
2024-06-09 19:08:40 +0200

    implemnet button leave exam for student

--- client/src/pages/OngoingExamPage/ExamWaitingPage.tsx
+++ client/src/pages/OngoingExamPage/ExamWaitingPage.tsx
@@ -6,20 +6,33 @@ import BaseTestInfo from '../../components/common/BaseTestInfo';
 import DottedText from '../../dev/DottedText/DottedText';
 import StudentsList from '../../components/common/StudentsList';
 import studentExamStore from '../../store/ExamStore/StudentExamStore';
+import Button from '../../components/UI/buttons/Button';
 
 interface Props {}
 
 const ExamWaitingPage: React.FC<Props> = observer(() => {
   const { exam } = studentExamStore;
 
+  const leaveExam = () => {
+    studentExamStore.resetExam();
+  };
+
   return (
     <QuizLayout>
       {exam?.test && (
         <BaseTestInfo
           test={exam.test}
-          action={<DottedText variant="h6">Waiting for the exam to start</DottedText>}
+          action={
+            <Button variant="outlined" color="error" onClick={leaveExam}>
+              Leave exam
+            </Button>
+          }
         />
       )}
+
+      <DottedText variant="h6" align="center">
+        Waiting for the exam to start
+      </DottedText>
 
       <Paper component={Stack} variant="outlined" spacing={4} padding={4}>
         {exam?.students?.length !== 0 && (
