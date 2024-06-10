8a5d0d765653a3f44a49754bfdcc3272f4d9f6a0
Sasza Nochovna <nochovna.sasha@gmail.com>
2024-06-10 10:18:42 +0200

    enabled theme change in settings page

--- client/src/components/SettingsOption.tsx
+++ client/src/components/SettingsOption.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Stack, StackProps } from '@mui/material';
+import { useThemeContext } from '../services/theme/ThemeContext';
 
 interface Props extends StackProps {
   name: React.ReactNode;
@@ -8,6 +9,7 @@ interface Props extends StackProps {
 }
 
 const SettingsOption: React.FC<Props> = ({ name, description, action, ...rest }) => {
+  const { toggleTheme } = useThemeContext();
   return (
     <Stack direction="row" justifyContent="space-between" alignItems="center" {...rest}>
       <Stack>
@@ -17,7 +19,7 @@ const SettingsOption: React.FC<Props> = ({ name, description, action, ...rest })
         </Typography>
       </Stack>
 
-      <Stack>{action}</Stack>
+      <Stack onClick={toggleTheme}>{action}</Stack>
     </Stack>
   );
 };
