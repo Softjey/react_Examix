7c9ccf29372676a0015e36eb69208f80051692e7
Junk-debug <maks.naumovich@gmail.com>
2024-07-02 22:49:37 +0300

    refactor create test page, move all logic to useCreateTestPage hook

--- client/src/components/CreateTestForm/CreateTestForm.tsx
+++ client/src/components/CreateTestForm/CreateTestForm.tsx
@@ -1,130 +1,38 @@
 import { Stack, TextField, Typography } from '@mui/material';
-import { FormProvider, useFieldArray, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useNavigate } from 'react-router';
-import { useRef, useState } from 'react';
-import {
-  CreateTestFormType,
-  CreateTestSchema,
-  QuestionFromServer,
-} from './schemas/createTestFormValidationSchemas';
-import defaultValues from '../../pages/CreateTestPage/defaultValues';
-import getDefaultQuestion from '../../pages/CreateTestPage/utils/getDefaultQuestion';
-import getFilteredQuestions from '../../pages/CreateTestPage/utils/getFilteredQuestions';
-import getPreparedTestQuestions from '../../pages/CreateTestPage/utils/getPreparedTestQuestions';
+import { FormProvider } from 'react-hook-form';
 import LoadingPage from '../../pages/LoadingPage';
-import { CreateTestDto } from '../../services/Api/types/create-test';
-import { Question } from '../../types/api/entities/question';
-import Subject from '../../types/api/enums/Subject';
-import { AvailableQuestionType } from '../../types/api/enums/Type';
 import QuestionsAutocompleteModal from '../UI/QuestionsAutoComplete/QuestionsAutocompleteModal';
 import LoadingButton from '../UI/buttons/LoadingButton';
 import TestInfo from './TestInfo';
 import FormQuestionList from './groups/FormQuestionList';
 import Button from '../UI/buttons/Button';
-import { useCreateTest } from '../../pages/CreateTestPage/CreateTestContext';
-import Routes from '../../services/Router/Routes';
-import useQuestions from '../../hooks/queries/useQuestions';
-import { Nullable } from '../../types/utils/Nullable';
 import AlertSnackbar from '../UI/AlertSnackbar';
+import useCreateTestPage from './useCreateTestPage';
 
 interface Props {}
 
 const CreateTestForm: React.FC<Props> = () => {
-  const navigate = useNavigate();
-
-  const { reset, loading, createQuestionsMutation, createTestMutation, error } = useCreateTest();
-  const { createQuestions } = createQuestionsMutation;
-  const { createTest } = createTestMutation;
-
-  const [search, setSearch] = useState<string>('');
-  const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
-
-  const [warningMessage, setWarningMessage] = useState<Nullable<string>>(null);
-
-  const { questions, ...restQueryParams } = useQuestions({
-    search: search || undefined,
-    limit: 20,
-  });
-
-  const { isLoading } = restQueryParams;
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
-  };
-
-  const methods = useForm<CreateTestFormType>({
-    resolver: zodResolver(CreateTestSchema),
-    defaultValues,
-    mode: 'onBlur',
-  });
-
-  const shouldScroll = useRef<boolean>(false);
-
-  const { fields, append, remove } = useFieldArray({
-    control: methods.control,
-    name: 'questions',
-  });
-
-  const addQuestionCard = () => {
-    append(getDefaultQuestion(), { shouldFocus: false });
-    shouldScroll.current = true;
-  };
-
-  const addQuestionCardFromServer = ({ type, ...question }: Question) => {
-    const { maxScore, timeLimit } = getDefaultQuestion();
-
-    const formQuestions = methods.watch('questions') as QuestionFromServer[];
-    const isDuplicate = formQuestions.some((formQuestion) => formQuestion.id === question.id);
-
-    if (isDuplicate) {
-      setWarningMessage('This question has already been added');
-      return;
-    }
-
-    append(
-      {
-        ...question,
-        type: type as AvailableQuestionType,
-        isFromServer: true,
-        maxScore,
-        timeLimit,
-      },
-      { shouldFocus: false },
-    );
-
-    shouldScroll.current = true;
-  };
-
-  const onSubmit = methods.handleSubmit((data) => {
-    const filteredQuestions = getFilteredQuestions(data);
-    const questionsFromServer = data.questions.filter((question) => question.isFromServer);
-
-    createQuestions(filteredQuestions, {
-      onSuccess: (createQuestionsResponse) => {
-        const testData: CreateTestDto = {
-          name: data.testName,
-          description: data.testDescription,
-          image: data.testImageLink,
-          questions: getPreparedTestQuestions(
-            createQuestionsResponse.questions,
-            questionsFromServer as QuestionFromServer[],
-            data.questions,
-          ),
-        };
-
-        if (data.subject) {
-          testData.subject = data.subject as Subject;
-        }
-
-        createTest(testData, {
-          onSuccess: (test) => {
-            navigate(`${Routes.TEST}/${test.id}`);
-          },
-        });
-      },
-    });
-  });
+  const {
+    loading,
+    isQuestionsAutocompleteLoading,
+    methods,
+    onSubmit,
+    shouldScroll,
+    fields,
+    remove,
+    addQuestionCard,
+    isModalOpened,
+    error,
+    reset,
+    warningMessage,
+    handleWarningClose,
+    questions,
+    search,
+    handleSearchChange,
+    openQuestionsLibraryModal,
+    closeQuestionsLibraryModal,
+    handleQuestionsAutocompleteChange,
+  } = useCreateTestPage();
 
   if (loading) {
     return <LoadingPage layout="home" />;
@@ -171,7 +79,7 @@ const CreateTestForm: React.FC<Props> = () => {
             color="secondary"
             disabled={loading}
             type="button"
-            onClick={() => setIsModalOpened(true)}
+            onClick={openQuestionsLibraryModal}
           >
             Add question from library
           </Button>
@@ -186,26 +94,17 @@ const CreateTestForm: React.FC<Props> = () => {
         {error?.message || 'Error occurred'}
       </AlertSnackbar>
 
-      <AlertSnackbar
-        severity="warning"
-        open={warningMessage !== null}
-        onClose={() => setWarningMessage(null)}
-      >
+      <AlertSnackbar severity="warning" open={warningMessage !== null} onClose={handleWarningClose}>
         {warningMessage}
       </AlertSnackbar>
 
       <QuestionsAutocompleteModal
         open={isModalOpened}
-        onClose={() => setIsModalOpened(false)}
+        onClose={closeQuestionsLibraryModal}
         autoCompleteProps={{
           options: questions || [],
-          onChange: (_, value) => {
-            addQuestionCardFromServer(value as Question);
-
-            setSearch('');
-            setIsModalOpened(false);
-          },
-          loading: isLoading,
+          onChange: handleQuestionsAutocompleteChange,
+          loading: isQuestionsAutocompleteLoading,
           renderInput: (params) => (
             <TextField
               autoFocus
---
+++ client/src/components/CreateTestForm/useCreateTestPage.ts
@@ -0,0 +1,156 @@
+import { useFieldArray, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useNavigate } from 'react-router';
+import { useRef, useState } from 'react';
+import {
+  CreateTestFormType,
+  CreateTestSchema,
+  QuestionFromServer,
+} from './schemas/createTestFormValidationSchemas';
+import defaultValues from '../../pages/CreateTestPage/defaultValues';
+import getDefaultQuestion from '../../pages/CreateTestPage/utils/getDefaultQuestion';
+import getFilteredQuestions from '../../pages/CreateTestPage/utils/getFilteredQuestions';
+import getPreparedTestQuestions from '../../pages/CreateTestPage/utils/getPreparedTestQuestions';
+import { CreateTestDto } from '../../services/Api/types/create-test';
+import { Question } from '../../types/api/entities/question';
+import Subject from '../../types/api/enums/Subject';
+import { AvailableQuestionType } from '../../types/api/enums/Type';
+import { useCreateTest } from '../../pages/CreateTestPage/CreateTestContext';
+import Routes from '../../services/Router/Routes';
+import useQuestions from '../../hooks/queries/useQuestions';
+import { Nullable } from '../../types/utils/Nullable';
+
+const useCreateTestPage = () => {
+  const navigate = useNavigate();
+
+  const { reset, loading, createQuestionsMutation, createTestMutation, error } = useCreateTest();
+  const { createQuestions } = createQuestionsMutation;
+  const { createTest } = createTestMutation;
+
+  const [search, setSearch] = useState<string>('');
+  const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
+
+  const [warningMessage, setWarningMessage] = useState<Nullable<string>>(null);
+
+  const { questions, isLoading: isQuestionsAutocompleteLoading } = useQuestions({
+    search: search || undefined,
+    limit: 20,
+  });
+
+  const methods = useForm<CreateTestFormType>({
+    resolver: zodResolver(CreateTestSchema),
+    defaultValues,
+    mode: 'onBlur',
+  });
+
+  const shouldScroll = useRef<boolean>(false);
+
+  const { fields, append, remove } = useFieldArray({
+    control: methods.control,
+    name: 'questions',
+  });
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  const addQuestionCard = () => {
+    append(getDefaultQuestion(), { shouldFocus: false });
+    shouldScroll.current = true;
+  };
+
+  const addQuestionCardFromServer = ({ type, ...question }: Question) => {
+    const { maxScore, timeLimit } = getDefaultQuestion();
+
+    const formQuestions = methods.watch('questions') as QuestionFromServer[];
+    const isDuplicate = formQuestions.some((formQuestion) => formQuestion.id === question.id);
+
+    if (isDuplicate) {
+      setWarningMessage('This question has already been added');
+      return;
+    }
+
+    append(
+      {
+        ...question,
+        type: type as AvailableQuestionType,
+        isFromServer: true,
+        maxScore,
+        timeLimit,
+      },
+      { shouldFocus: false },
+    );
+
+    shouldScroll.current = true;
+  };
+
+  const onSubmit = methods.handleSubmit((data) => {
+    const filteredQuestions = getFilteredQuestions(data);
+    const questionsFromServer = data.questions.filter((question) => question.isFromServer);
+
+    createQuestions(filteredQuestions, {
+      onSuccess: (createQuestionsResponse) => {
+        const testData: CreateTestDto = {
+          name: data.testName,
+          description: data.testDescription,
+          image: data.testImageLink,
+          questions: getPreparedTestQuestions(
+            createQuestionsResponse.questions,
+            questionsFromServer as QuestionFromServer[],
+            data.questions,
+          ),
+        };
+
+        if (data.subject) {
+          testData.subject = data.subject as Subject;
+        }
+
+        createTest(testData, {
+          onSuccess: (test) => {
+            navigate(`${Routes.TEST}/${test.id}`);
+          },
+        });
+      },
+    });
+  });
+
+  const openQuestionsLibraryModal = () => setIsModalOpened(true);
+
+  const closeQuestionsLibraryModal = () => setIsModalOpened(false);
+
+  const handleQuestionsAutocompleteChange = (
+    _: React.SyntheticEvent<Element, Event>,
+    value: NonNullable<string | Question> | (string | Question)[] | null,
+  ) => {
+    addQuestionCardFromServer(value as Question);
+
+    setSearch('');
+    setIsModalOpened(false);
+  };
+
+  const handleWarningClose = () => setWarningMessage(null);
+
+  return {
+    loading,
+    isQuestionsAutocompleteLoading,
+    methods,
+    onSubmit,
+    shouldScroll,
+    fields,
+    remove,
+    addQuestionCard,
+    isModalOpened,
+    error,
+    reset,
+    warningMessage,
+    handleWarningClose,
+    questions,
+    search,
+    handleSearchChange,
+    openQuestionsLibraryModal,
+    closeQuestionsLibraryModal,
+    handleQuestionsAutocompleteChange,
+  };
+};
+
+export default useCreateTestPage;
