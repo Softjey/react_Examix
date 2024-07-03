efb34ca658252600f0d561fa70b494dd9f3a21dd
Junk-debug <maks.naumovich@gmail.com>
2024-07-03 10:02:04 +0300

    change condition on disable kick student button

--- client/src/pages/OngoingExamPanelPage.tsx
+++ client/src/pages/OngoingExamPanelPage.tsx
@@ -86,7 +86,7 @@ const OngoingExamPanelPage: React.FC<Props> = observer(({ ...rest }) => {
       <QuestionsList variant="accordion" questions={test.testQuestions} />
 
       <StudentsList
-        disableKickButton={status === 'started'}
+        disableKickButton={status !== 'created'}
         variant="accordion"
         students={students ?? []}
         onKick={({ studentId }) => kickStudent(studentId)}
